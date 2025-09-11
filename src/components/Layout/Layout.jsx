0165ef4336cebd0396af7006dbbb442c71f89615
Rockey <rkrockey9690@gmail.com>
2025-09-11 12:57:49 +0530

    commit-chg

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Routers from '../../router/Router'
+import { ToastContainer } from 'react-toastify'
 
 const Layout = () => {
   return (
     <>
+    <ToastContainer position="top-right" autoClose={2000} />
     <Header/>
     <Routers/>
     <Footer/>
